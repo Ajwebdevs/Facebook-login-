8f38ca6ce916749026a573898bd41aa2faa5b4c1
ARJUN S NAIR <5minutepodcastforyou@gmail.com>
2024-07-05 10:26:12 +0530

    privacy

--- src/Facebookbtn.jsx
+++ src/Facebookbtn.jsx
@@ -1,42 +1,12 @@
-// import React from 'react';
-// import FacebookLogin from 'react-facebook-login';
-
-// const FacebookLoginButton = ({ setUser }) => {
-//     const responseFacebook = (response) => {
-//         console.log(response);  
-//         if (response.accessToken) {
-//             setUser({
-//                 name: response.name,
-//                 picture: response.picture.data.url,
-//                 accessToken: response.accessToken,
-//             });
-//         } else {
-//             console.error('User not allowed to use the follwing resources !');
-//         }
-//     };
-
-//     return (
-//         <FacebookLogin
-//             appId="994007258933295"
-//             autoLoad={true}
-//             fields="name,picture"
-//             callback={responseFacebook}
-//         />
-//     );
-// };
-
-// export default FacebookLoginButton;
-
 import React, { useEffect } from 'react';
 
 const FacebookLoginButton = ({ setUser }) => {
   useEffect(() => {
     window.fbAsyncInit = function () {
       FB.init({
-        appId: '994007258933295',
-        cookie: true,
-        xfbml: true,
-        version: 'v14.0'
+        appId      : '1065509685190294',
+        xfbml      : true,
+        version    : 'v20.0'
       });
     };
 //new version
